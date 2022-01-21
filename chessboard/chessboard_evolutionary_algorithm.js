9854ae7e90850d8fa617d8d0ec18dac12a2e3710
Alex D <dockhorn.alexander@gmail.com>
2022-01-21 23:04:03 +0100

    added row and columnvector EA

---
+++ chessboard/chessboard_evolutionary_algorithm.js
@@ -0,0 +1,157 @@
+import {highlightCollisions, example_positions, position_to_binary_board} from "./chessboard_utils.js";
+import {RowVectorIndividual, validateRowVectorIndividual} from "./rowVectorIndiduals.js"
+import {ColumnVectorIndividual, validateColumnVectorIndividual} from "./columnVectorIndividuals.js"
+
+let board = Chessboard('board-ea-test', {
+    draggable: false,
+    position: example_positions[0],
+});
+
+let $board = $('#board-ea-test');
+
+position_to_binary_board(board.position());
+
+//runEvolutionaryAlgorithm(board);
+
+document.getElementById("awesome").addEventListener("click", doStuff);
+
+function doStuff(){
+    //validateColumnVectorIndividual()
+
+    let representationName = $('#select_representation').find(":selected").text();
+    let mutationName = $('#select_mutation').find(":selected").text();
+    let crossoverName = $('#select_crossover').find(":selected").text();
+
+    let repetitions = 10;
+    let solvedRuns = 0;
+    let totalGenerations = 0;
+    for (let i = 0; i < repetitions; i++){
+        let result = runEvolutionaryAlgorithm(representationName, mutationName, crossoverName);
+        if (result[0]) solvedRuns += 1;
+        totalGenerations += result[1];
+    }
+    console.log(representationName, mutationName, crossoverName, solvedRuns, totalGenerations/repetitions);
+
+}
+
+function updateGUI(){
+    var $selectRepresentation = $("#select_representation");
+    var $selectMutationion = $("#select_mutation");
+    var $selectCrossover = $("#select_crossover");
+
+    var options = [
+        {text: "one", value: 1},
+        {text: "two", value: 2}
+    ];
+
+    $("#foo").replaceOptions(options);
+}
+
+
+function runEvolutionaryAlgorithm(representationName, mutationName, crossoverName){
+    /*console.log(representationName);
+    console.log(mutationName);
+    console.log(crossoverName);*/
+
+    // parameters
+    let populationsize = 10;
+    let maxGenerations = 1000;
+    let generations = [];
+
+    // initialization
+    let currentPopulation = initializePopulation(populationsize, representationName, mutationName, crossoverName);
+    generations[0] = currentPopulation;
+
+    //
+    function checkSolved(population) {
+        let solved = false;
+        population.forEach(individual => {
+            if (individual.fitness === 8){
+                solved = true;
+            }
+        })
+        return solved;
+    }
+
+    while (checkSolved(currentPopulation) === false && generations.length !== maxGenerations) {
+        //console.log(currentPopulation.map(el => el.getFitness()))
+        let mutationChilds = []
+
+        let selectedIndividuals = tournamentSelection(currentPopulation, populationsize);
+        selectedIndividuals.forEach(el => {
+            let newEl = el.clone()
+            newEl.mutation();
+            mutationChilds[mutationChilds.length] = newEl;
+        })
+
+        let crossOverChilds = []
+        for (let i = 0; i < 10; i++){
+            let parent1 = selectedIndividuals[Math.floor(Math.random()*10)];
+            let parent2 = selectedIndividuals[Math.floor(Math.random()*10)];
+
+            crossOverChilds[crossOverChilds.length] = parent1.crossover(parent2);
+        }
+
+        let children = mutationChilds.concat(crossOverChilds);
+        children.sort((a,b) => (a.fitness > b.fitness) ? -1 : ((b.fitness > a.fitness) ? 1 : 0))
+        //console.log("children: " + children.map(el => el.getFitness()))
+
+        currentPopulation = children.slice(0, populationsize);
+        generations[generations.length] = currentPopulation;
+    }
+    //console.log("currentPopulation: " + currentPopulation.map(el => el.fitness))
+    //console.log("the puzzle has been solved: " + checkSolved(currentPopulation) +" after " + generations.length +" generations");
+    return [checkSolved(currentPopulation), generations.length]
+}
+
+
+function tournamentSelection(population, populationsize) {
+    let tournamentsize = 4;
+    let selectedIndividuals = [];
+
+    function getRandomSubarray(arr, size) {
+        var shuffled = arr.slice(0), i = arr.length, temp, index;
+        while (i--) {
+            index = Math.floor((i + 1) * Math.random());
+            temp = shuffled[index];
+            shuffled[index] = shuffled[i];
+            shuffled[i] = temp;
+        }
+        return shuffled.slice(0, size);
+    }
+
+    for (let i = 0; i < populationsize; i++){
+        let tournament = getRandomSubarray(population, tournamentsize);
+        tournament.sort((a,b) => (a.fitness > b.fitness) ? -1 : ((b.fitness > a.fitness) ? 1 : 0))
+        selectedIndividuals[i] = tournament[0].clone();
+    }
+    return selectedIndividuals;
+}
+
+function initializePopulation(populationsize, representationName, mutationName, crossoverName){
+    let population = []
+    for (let i = 0; i < populationsize; i++){
+        switch (representationName){
+            default:
+                population[i] = new ColumnVectorIndividual(mutationName, crossoverName);
+                break;
+        }
+    }
+    return population
+}
+
+
+// INDIVIDUALS
+
+
+//---------------------------------------------
+
+class PermutationVectorIndividual {
+
+}
+
+//---------------------------------------------
+
+class ConstrainedMatrixIndividual {
+
+}
---
+++ chessboard/columnVectorIndividuals.js
@@ -0,0 +1,245 @@
+import {position_to_binary_board} from "./chessboard_utils.js";
+
+/**
+ * Shuffles array in place.
+ * @param {Array} a items An array containing the items.
+ */
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+
+export let ColumnVectorMutationOptions = [
+    "Value Mutation",
+    "Swap Mutation",
+    "Inversion Mutation",
+    "Shuffle Mutation"
+];
+
+export let ColumnVectorCrossoverOptions = [
+    "1-Point Crossover",
+    "2-Point Crossover",
+    "Uniform Crossover (p=0.5)"
+]
+
+let colNames = ["a", "b", "c", "d", "e", "f", "g", "h"]
+let possibleValues = [1, 2, 3, 4, 5, 6, 7, 8]
+
+export class ColumnVectorIndividual {
+
+    constructor(mutationName, crossoverName) {
+        this.values = this.generateRandomIndividual();
+        this.fitness = this.getFitness();
+
+        this.selectMutation(mutationName);
+        this.selectCrossover(crossoverName);
+
+        return this;
+    }
+
+    clone() {
+        let newIndividual = new ColumnVectorIndividual(this.mutationName, this.crossoverName);
+        for (let i = 0; i < 8; i++){
+            newIndividual.values[i] = this.values[i];
+        }
+        newIndividual.fitness = this.fitness;
+
+        return newIndividual;
+    }
+
+    selectMutation(mutationName){
+        this.mutationName = mutationName;
+        switch (mutationName){
+            case "Value Mutation" :
+                this.mutation = this.valueMutation;
+                break;
+            case "Swap Mutation" :
+                this.mutation = this.swapMutation;
+                break;
+            case "Inversion Mutation" :
+                this.mutation = this.inversionMutation;
+                break;
+            case "Shuffle Mutation" :
+                this.mutation = this.shuffleMutation;
+                break;
+        }
+    }
+
+    selectCrossover(crossoverName){
+        this.crossoverName = crossoverName;
+        switch (crossoverName){
+            case "1-Point Crossover" :
+                this.crossover = this.onepointCrossover;
+                break;
+            case "2-Point Crossover" :
+                this.crossover = this.twoPointCrossover;
+                break;
+            case "Uniform Crossover (p=0.5)" :
+                this.crossover = this.uniformCrossover;
+                break;
+        }
+    }
+
+    getFitness(){
+        const [_, unsafe_positions, safe_positions] = position_to_binary_board(this.getPosition());
+        return safe_positions.length;
+    }
+
+    getPosition(){
+        let position = {}
+        this.values.forEach((el, i) => position[""+colNames[i]+el] = "bQ")
+        return position;
+    }
+
+    generateRandomIndividual(){
+        let values = []
+        for (let i = 0; i < 8; i++){
+            values[i] = possibleValues[Math.floor(Math.random() * 8)]
+        }
+        return values;
+    }
+
+    valueMutation(){
+        let randomIndex = Math.floor(Math.random() * 8);
+        this.values[randomIndex] = possibleValues[Math.floor(Math.random()*8)]
+        this.fitness = this.getFitness();
+    }
+
+    swapMutation(){
+        let randomIndex = Math.floor(Math.random() * 8);
+        let randomIndex2 = Math.floor(Math.random() * 8);
+        while (randomIndex2 === randomIndex){
+            randomIndex2 = Math.floor(Math.random() * 8);
+        }
+        let a = this.values[randomIndex2];
+        this.values[randomIndex2] = this.values[randomIndex];
+        this.values[randomIndex] = a;
+        this.fitness = this.getFitness();
+    }
+
+    inversionMutation(){
+        let randomIndex = Math.floor(Math.random() * 7);
+        let randomIndex2 = 1+Math.floor(Math.random() * 8);
+        while (randomIndex2 <= randomIndex){
+            randomIndex2 = 1+Math.floor(Math.random() * 8);
+        }
+        let slice = this.values.slice(randomIndex, randomIndex2);
+        slice.reverse();
+
+        for (let i = randomIndex; i<randomIndex2; i++) {
+            this.values[i] = slice[i - randomIndex];
+        }
+        this.fitness = this.getFitness();
+    }
+
+    shuffleMutation(){
+        let randomIndex = Math.floor(Math.random() * 7);
+        let randomIndex2 = 1+Math.floor(Math.random() * 8);
+        while (randomIndex2 <= randomIndex){
+            randomIndex2 = 1+Math.floor(Math.random() * 8);
+        }
+        let slice = this.values.slice(randomIndex, randomIndex2);
+        shuffle(slice);
+
+        for (let i = randomIndex; i<randomIndex2; i++) {
+            this.values[i] = slice[i - randomIndex];
+        }
+        this.fitness = this.getFitness();
+    }
+
+    onepointCrossover(other){
+        let child = this.clone();
+
+        let cutoffPoint = 1+Math.floor(Math.random()*6);
+        for (let i = 0; i < 8; i++){
+            child.values[i] = i < cutoffPoint ? this.values[i] : other.values[i];
+        }
+        child.fitness = child.getFitness();
+        return child;
+    }
+
+    twoPointCrossover(other){
+        let child = this.clone();
+        let cutoffPoint = 1+Math.floor(Math.random()*5);
+        let cutoffPoint2 = cutoffPoint+1+Math.floor(Math.random()*(6-cutoffPoint));
+        for (let i = 0; i < 8; i++){
+            child.values[i] = i < cutoffPoint || i > cutoffPoint2 ? this.values[i] : other.values[i];
+        }
+        child.fitness = child.getFitness();
+        return child;
+    }
+
+    uniformCrossover(other){
+        let child = this.clone();
+        for (let i = 0; i < 8; i++){
+            child.values[i] = Math.random() < 0.5 ? this.values[i] : other.values[i];
+        }
+        child.fitness = child.getFitness();
+        return child;
+    }
+
+
+}
+
+export function validateColumnVectorIndividual() {
+    console.log("test generation")
+    let a = new ColumnVectorIndividual();
+
+    console.log("test inversion")
+    for (let i = 0; i < 100; i++){
+        a.inversionMutation()
+        console.log(a.values)
+    }
+
+    console.log("test shuffle")
+    for (let i = 0; i < 100; i++){
+        a.shuffleMutation()
+        console.log(a.values)
+    }
+
+    console.log("test value")
+    for (let i = 0; i < 100; i++){
+        a.valueMutation()
+        console.log(a.values)
+    }
+
+    console.log("test swap")
+    for (let i = 0; i < 100; i++){
+        a.swapMutation()
+        console.log(a.values)
+    }
+
+    let b = new ColumnVectorIndividual();
+    a.values = [1,1,1,1,1,1,1,1]
+    b.values = [2,2,2,2,2,2,2,2]
+
+    console.log("test 1pointCrossover")
+    for (let i = 0; i < 20; i++){
+        console.log(a.onepointCrossover(b).values)
+    }
+
+    console.log("test twoPointCrossover")
+    for (let i = 0; i < 20; i++){
+        console.log(a.twoPointCrossover(b).values)
+    }
+
+    console.log("test uniformCrossover")
+    for (let i = 0; i < 20; i++){
+        console.log(a.uniformCrossover(b).values)
+    }
+
+    console.log("test getPosition and getFitness")
+    for (let i = 0; i < 10; i++){
+        let a = new ColumnVectorIndividual();
+        console.log(a.values);
+        console.log(a.getPosition());
+        console.log(a.getFitness());
+    }
+
+}
